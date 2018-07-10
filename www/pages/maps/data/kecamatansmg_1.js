241c407e52da49a6c8050683e469f653c9951fb4
ABBAH <amaychifa@gmail.com>
2018-07-11 01:08:00 +0700

    awal

---
+++ www/pages/maps/data/kecamatansmg_1.js
@@ -0,0 +1,23 @@
+var json_kecamatansmg_1 = {
+"type": "FeatureCollection",
+"name": "kecamatansmg_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "15", "kecamatan": "Tugu" }, "geometry": { "type": "Point", "coordinates": [ 110.337411322247107, -6.971420791759035 ] } },
+{ "type": "Feature", "properties": { "id": "14", "kecamatan": "Ngaliyan" }, "geometry": { "type": "Point", "coordinates": [ 110.319630182253661, -7.001571420443556 ] } },
+{ "type": "Feature", "properties": { "id": "16", "kecamatan": "Mijen" }, "geometry": { "type": "Point", "coordinates": [ 110.321949461383241, -7.061099584769403 ] } },
+{ "type": "Feature", "properties": { "id": "12", "kecamatan": "Gunung_pati" }, "geometry": { "type": "Point", "coordinates": [ 110.372200509190776, -7.064191956942174 ] } },
+{ "type": "Feature", "properties": { "id": "10", "kecamatan": "Banyumanik" }, "geometry": { "type": "Point", "coordinates": [ 110.408149335699235, -7.066704509332551 ] } },
+{ "type": "Feature", "properties": { "id": "11", "kecamatan": "Genuk" }, "geometry": { "type": "Point", "coordinates": [ 110.467194316873162, -6.969657173254252 ] } },
+{ "type": "Feature", "properties": { "id": "9", "kecamatan": "Pedurungan" }, "geometry": { "type": "Point", "coordinates": [ 110.457144107311649, -7.010631104543472 ] } },
+{ "type": "Feature", "properties": { "id": "2", "kecamatan": "Semarang_utara" }, "geometry": { "type": "Point", "coordinates": [ 110.409212338633694, -6.965018614995095 ] } },
+{ "type": "Feature", "properties": { "id": "8", "kecamatan": "Candisari" }, "geometry": { "type": "Point", "coordinates": [ 110.417523088848014, -7.022903956604159 ] } },
+{ "type": "Feature", "properties": { "id": "7", "kecamatan": "Gajah_mungkur" }, "geometry": { "type": "Point", "coordinates": [ 110.396456303421019, -7.018072125084204 ] } },
+{ "type": "Feature", "properties": { "id": "4", "kecamatan": "Semarang_selatan" }, "geometry": { "type": "Point", "coordinates": [ 110.41423744341445, -7.005316089871521 ] } },
+{ "type": "Feature", "properties": { "id": "1", "kecamatan": "Semarang_tengah" }, "geometry": { "type": "Point", "coordinates": [ 110.409405611894499, -6.989081135964472 ] } },
+{ "type": "Feature", "properties": { "id": "3", "kecamatan": "Semarang_barat" }, "geometry": { "type": "Point", "coordinates": [ 110.375969337776411, -6.990240775529261 ] } },
+{ "type": "Feature", "properties": { "id": "5", "kecamatan": "Semarang_timur" }, "geometry": { "type": "Point", "coordinates": [ 110.424094379715186, -6.983959394553319 ] } },
+{ "type": "Feature", "properties": { "id": "6", "kecamatan": "Gayamsari" }, "geometry": { "type": "Point", "coordinates": [ 110.4331782229727, -6.989757592377266 ] } },
+{ "type": "Feature", "properties": { "id": "13", "kecamatan": "Tembalang" }, "geometry": { "type": "Point", "coordinates": [ 110.456437758389612, -7.053484309879178 ] } }
+]
+}
